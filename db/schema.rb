b1e33f37c7264ada768624cd9fb3a2ee11875b8f
gedunlap <g.e.dunlap90@gmail.com>
2021-06-25 13:14:37 -0400

    rollback destroy posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_25_170945) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "posts", force: :cascade do |t|
-    t.string "name"
-    t.string "contact"
-    t.string "desc"
-    t.string "image"
-    t.string "video"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
 end
