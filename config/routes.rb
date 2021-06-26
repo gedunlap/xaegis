bf66b7a459c8cd7f298b64dc09a7f9e5a06a697a
gedunlap <g.e.dunlap90@gmail.com>
2021-06-26 13:38:04 -0400

    setup login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
+  resource :users, only: [:create]
+  post "/login", to: "users#login"
+  get "/auto_login", to: "user#auto_login"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
