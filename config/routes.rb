78eebc5098b32174ea0a727dd81579fac6e0c48c
gedunlap <g.e.dunlap90@gmail.com>
2021-06-26 15:34:02 -0400

    fixes users error in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :posts
   resource :users, only: [:create]
   post "/login", to: "users#login"
-  get "/auto_login", to: "user#auto_login"
+  get "/auto_login", to: "users#auto_login"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
